9de5d0082b6accb2262803370b58d8cfcd27a5e7
Artemis <artemis@Artemis.local>
2022-12-06 20:43:19 -0600

    updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react"
-import Navbar from "../components/Navbar"
-import Hero from "../components/Hero"
-import Card from "../components/Card"
-import data from "../public/data"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import data from "./data"
 
 export default function App() {
     const cards = data.map(item => {
