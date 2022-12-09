6874033cf39b1770dfbdef6663fe0b230cedc0a7
Artemis <artemis@Artemis.local>
2022-12-08 18:05:35 -0600

    styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,8 @@ import React from "react"
 export default function Navbar() {
     return (
         <nav>
-            <img src="../images/airbnb-logo.png" className="nav--logo" />
+            {/* <img src="../images/airbnb-logo.png" className="nav--logo" /> */}
+            <h1>New navbar</h1>
         </nav>
     )
 }
